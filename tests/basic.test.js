f6939a13996df64f350a6ab92a2e31d42b73962e
yinyong <yanni4night@gmail.com>
2014-11-19 17:14:05 +0800

    update version test

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -13,8 +13,8 @@ function resetOptions() {
 }
 
 describe('version', function () {
-  it('is 1.4.2', function () {
-    expect(swig.version).to.equal('1.4.2');
+  it('is 1.0.142', function () {
+    expect(swig.version).to.equal('1.0.142');
   });
 });
 
